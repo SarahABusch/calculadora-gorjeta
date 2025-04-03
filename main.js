8cca489f48c67e556602f017b8f76467666d34e8
SarahABusch <renata_gomes88@hotmail.com>
2025-04-02 22:17:25 -0300

    funções para receber o valor da cota e a quantidade de pessoas no JS

---
+++ main.js
@@ -0,0 +1,29 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+let billInput = document.querySelector("#bill")
+billInput.addEventListener("input", reciveBillValue)
+
+let numberOfPeopleInput = document.querySelector("#people")
+numberOfPeopleInput.addEventListener("input", reciveNumberOfPeopleValue )
+
+function reciveBillValue(){
+ bill = billInput.valueAsNumber
+ caculate()
+}
+
+function reciveNumberOfPeopleValue() {
+ numberOfPeople = numberOfPeopleInput.valueAsNumber
+ caculate()
+}
+
+function caculate() {
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+        console.log ("É possível calcular")
+    }
+    else {
+        console.log("Não é possível calcular")  
+    }
+    
+}
